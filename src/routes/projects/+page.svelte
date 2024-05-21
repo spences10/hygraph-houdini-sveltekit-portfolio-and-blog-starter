9965a8e2af4745378113724008e7215271e60b7d
spences10 <spences10apps@gmail.com>
2024-05-21 20:35:25 +0100

    refactor: :recycle: add 'projects' route

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { ProjectCard } from '$lib/componentes';
+	import type { PageData } from './$houdini';
+
+	let { data }: { data: PageData } = $props();
+
+	let page_data = data.projectsData;
+</script>
+
+<h1 class="mb-20 text-center text-5xl font-bold">
+	Recent Projects by Me
+</h1>
+
+<div
+	class="grid gap-10 md:grid-cols-4 md:px-10 lg:-mx-52 lg:grid-cols-6"
+>
+	{#if $page_data.data && $page_data.data.projects}
+		{#each $page_data.data.projects as project}
+			<ProjectCard {project} />
+		{/each}
+	{/if}
+</div>
---
+++ src/routes/projects/[slug]/+page.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { marked } from 'marked';
+	import type { PageData } from './$houdini';
+
+	let { data }: { data: PageData } = $props();
+
+	let page_data = data.projectData;
+</script>
+
+<div class="xl:-mx-38 mb-5 sm:-mx-5 md:-mx-10 lg:-mx-20">
+	<img
+		class="rounded-lg"
+		src={$page_data.data?.project?.image[0].url}
+		alt={$page_data.data?.project?.name}
+	/>
+</div>
+
+<h1 class="mb-5 text-4xl font-semibold">
+	{$page_data.data?.project?.name}
+</h1>
+
+<div class="mb-5 flex justify-between">
+	<div>
+		{#if $page_data.data?.project?.tags}
+			{#each $page_data.data?.project?.tags as tag}
+				<span
+					class="hover:bg-primary-focus badge badge-primary mr-2 cursor-pointer"
+				>
+					{tag}
+				</span>
+			{/each}
+		{/if}
+	</div>
+</div>
+
+<div
+	class="hover:prose-a:text-primary-focus prose mb-5 flex prose-a:text-primary"
+>
+	<a class="mr-5" href={$page_data.data?.project?.demo}>Demo</a>
+	<a href={$page_data.data?.project?.sourceCode}>Source Code</a>
+</div>
+
+<article class="prose prose-xl">
+	{@html marked($page_data.data?.project?.description ?? '')}
+</article>
