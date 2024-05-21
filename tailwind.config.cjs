8cfe491368ac9fc9509e03af9691b2a2324c6b22
spences10 <spences10apps@gmail.com>
2024-05-21 19:57:42 +0100

    feat: :sparkles: add/config tailwind daisyUI

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+const daisyui = require('daisyui');
+const typography = require('@tailwindcss/typography');
+
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {
+			typography: {
+				DEFAULT: {
+					css: {
+						maxWidth: null,
+					},
+				},
+			},
+		},
+	},
+
+	plugins: [typography, daisyui],
+};
+
+module.exports = config;
